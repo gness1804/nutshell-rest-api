6657b9ce8711cbfcbc85c9f008aa14b8779c5b1b
gness1804 <graham.nessler@gmail.com>
2017-03-16 17:55:05 -0600

    Server now dynamically serves up page content

--- public/scripts.js
+++ public/scripts.js
@@ -3,10 +3,13 @@ const mainButton = document.querySelector('#main-button')
 const getPeople = () => {
   const data = []
   const makeAPICall = (i) => {
-      axios.get(`/getpeople/:i`).then((response) => {console.log(response)})
+    axios.get(`/getpeople/${i}`) // eslint-disable-line
+      .then((response) => {
+        console.log(response)
+      })
   }
-  let counter = 1
-  for (var i = 0; i < 3; i++) {
+  // let counter = 1
+  for (let i = 1; i < 4; i++) {
     makeAPICall(i)
   }
   return data
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const http = require('http');
+// const http = require('http');
 const axios = require('axios');
 
 app.use(express.static('public'));
@@ -18,16 +19,16 @@ app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const hitAPI = () => {
-  return axios.get('http://join.nutshell.com/people/1/')
+const hitAPI = (number) => {
+  return axios.get(`http://join.nutshell.com/people/${number}/`)
 }
 
 app.get('/getpeople/:i', (request, response) => {
-  hitAPI().then((data) => {
+  hitAPI(request.params.i).then((data) => {
     response.send(data.data)
   })
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Server running on port 3000.');
+    console.log('Server running on port 3000.'); // eslint-disable-line
 });
