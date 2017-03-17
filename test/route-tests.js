7e291020213c35c8562b3004bdb142ef720b5ab6
gness1804 <graham.nessler@gmail.com>
2017-03-16 21:24:10 -0600

    First route test passes

---
+++ test/route-tests.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+const jsdom = require('mocha-jsdom');
+
+const chai = require('chai');
+const assert = require('assert');
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+const expect = chai.expect();
+const should = chai.should();
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp)
+
+describe('get /', function () {
+  it('should return a 200 status message', function () {
+    request(app)
+      .get('/')
+      .end(function(error, result) {
+        result.should.have.status(200);
+        done();
+      });
+  });
+});
