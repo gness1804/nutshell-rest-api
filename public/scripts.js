3a04132f8216964f7d043ef1d8180cceda2bf19c
gness1804 <graham.nessler@gmail.com>
2017-03-16 20:04:24 -0600

    Finally got the right data to output for all API calls

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,32 @@
 const mainButton = document.querySelector('#main-button')
+const output = []
+
+const pushObjectsIntoMasterArray = (data) => {
+  if (typeof data !== 'undefined') {
+    data.forEach((obj) => {
+      output.push(obj)
+    });
+  }
+}
+
+const findFiveMostRecentPeople = (data) => {
+  return data
+}
 
 const getPeople = () => {
-  const data = []
   const makeAPICall = (i) => {
     axios.get(`/getpeople/${i}`) // eslint-disable-line
       .then((response) => {
-        console.log(response)
+        if (response.data.length) {
+          return response.data
+        }
       })
+      .then((data) => { pushObjectsIntoMasterArray(data) })
+      .then(() => { findFiveMostRecentPeople(output) })
   }
-  // let counter = 1
-  for (let i = 1; i < 4; i++) {
+  for (let i = 1; i < 50; i++) {
     makeAPICall(i)
   }
-  return data
 }
 
 mainButton.addEventListener('click', () => {
